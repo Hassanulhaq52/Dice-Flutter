06b8c4302f10dfb64e6afeb615188349b9b30cf4
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-29 18:20:58 +0500

    Making the Dice Image Change Reactively

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,9 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    var leftDiceNumber = 6;
+
     return Center(
       child: Row(
         children: [
@@ -33,7 +36,7 @@ class DicePage extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Image.asset(
-                    'images/dice1.png',
+                    'images/dice$leftDiceNumber.png',
                 ),
               ),
             ),
